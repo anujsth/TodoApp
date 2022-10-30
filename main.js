84cfb51aa41272686e5551bca3ff5b412ee2da3d
Anuj Shrestha <anujshrestha.as22@gmail.com>
2022-10-30 20:25:01 +0545

    listing_tasks

--- main.js
+++ main.js
@@ -4,5 +4,14 @@ const list_el = document.querySelector("#tasks");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log("submitted");
+  const task = input.value;
+
+  if (task) {
+  } else {
+    alert("Please enter task");
+    return;
+  }
+
+  const task_el = document.createElement("div");
+  task_el.classList.add("task");
 });
