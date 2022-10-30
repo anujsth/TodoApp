f11984b1570e4b2ccce779c05d0681ef9aba6997
Anuj Shrestha <anujshrestha.as22@gmail.com>
2022-10-30 20:26:12 +0545

    list_added

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
-const form = document.querySelector("#new-task-form");
-const input = document.querySelector("#new-task-input");
-const list_el = document.querySelector("#tasks");
+window.addEventListener("load", () => {
+  const form = document.querySelector("#new-task-form");
+  const input = document.querySelector("#new-task-input");
+  const list_el = document.querySelector("#tasks");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const task = input.value;
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const task = input.value;
 
-  if (task) {
-  } else {
-    alert("Please enter task");
-    return;
-  }
+    if (!task) {
+      alert("Please enter task");
+      return;
+    }
+    const task_el = document.createElement("div");
+    task_el.classList.add("task");
 
-  const task_el = document.createElement("div");
-  task_el.classList.add("task");
+    const task_content_el = document.createElement("div");
+    task_content_el.classList.add("content");
+    task_content_el.innerText = task;
+
+    task_el.appendChild(task_content_el);
+
+    list_el.appendChild(task_el);
+  });
 });
